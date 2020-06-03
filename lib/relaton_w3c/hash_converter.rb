7f20892f85a9c1bcfbfbcd21fb808dd0eca2871a
Andrei Kislichenko <android.2net@gmail.com>
2020-06-03 19:55:32 +0200

    update grammar

--- lib/relaton_w3c/hash_converter.rb
+++ lib/relaton_w3c/hash_converter.rb
@@ -1,4 +1,11 @@
 module RelatonW3c
   class HashConverter < RelatonBib::HashConverter
+    class << self
+      # @param item_hash [Hash]
+      # @return [RelatonW3c::W3cBibliographicItem]
+      def bib_item(item_hash)
+        W3cBibliographicItem.new item_hash
+      end
+    end
   end
 end
--- lib/relaton_w3c/xml_parser.rb
+++ lib/relaton_w3c/xml_parser.rb
@@ -1,19 +1,6 @@
 module RelatonW3c
   class XMLParser < RelatonBib::XMLParser
     class << self
-      # @param xml [String]
-      # @return [RelatonW3c::W3cBibliographicItem, NilClass]
-      def from_xml(xml)
-        doc = Nokogiri::XML xml
-        doc.remove_namespaces!
-        item = doc.at("/bibitem|/bibdata")
-        if item
-          W3cBibliographicItem.new(item_data(item))
-        else
-          warn "[relaton-w3c] can't find bibitem or bibdata element in the XML"
-        end
-      end
-
       private
 
       # Override RelatonBib::XMLParser.item_data method.
@@ -26,6 +13,12 @@ module RelatonW3c
 
         data[:doctype] = ext.at("./doctype")&.text
         data
+      end
+
+      # @param item_hash [Hash]
+      # @return [RelatonBib::BibliographicItem]
+      def bib_item(item_hash)
+        W3cBibliographicItem.new item_hash
       end
     end
   end
