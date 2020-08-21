24491fa9e340039330ef838f9cfa916b6e7f24ce
Andrei Kislichenko <android.2net@gmail.com>
2020-08-21 11:30:01 +0200

    bump new version

--- lib/relaton_w3c/w3c_bibliographic_item.rb
+++ lib/relaton_w3c/w3c_bibliographic_item.rb
@@ -5,35 +5,12 @@ module RelatonW3c
       proposedRecommendation recommendation retired workingDraft
     ].freeze
 
-    attr_reader :doctype
-
     # @param doctype [String]
     def initialize(**args)
       if args[:doctype] && !TYPES.include?(args[:doctype])
         warn "[relaton-w3c] invalid document type: #{args[:doctype]}"
       end
       super **args
-      # @doctype = args[:doctype]
-    end
-
-    # @param builder [Nokogiri::XML::Builder, NilClass]
-    # @param opts [Hash]
-    # @option opts [TrueClass, FalseClass, NilClass] bibdata
-    def to_xml(builder = nil, **opts)
-      super builder, **opts do |b|
-        if opts[:bibdata] && doctype
-          b.ext do |e|
-            e.doctype doctype if doctype
-          end
-        end
-      end
-    end
-
-    # @return [Hash]
-    def to_hash
-      hash = super
-      hash["doctype"] = doctype if doctype
-      hash
     end
   end
 end
